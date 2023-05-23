e0cec0096f075bcd1a55b85e660435abf4896ec8
hvlopez <hectorvmlopez@gmail.com>
2023-05-22 22:20:25 -0600

    add

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,18 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useEffect, useRef } from "react";
 
-export default () => {
+export default ({ history }) => {
   const ref = useRef(null);
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
   }, []);
   return <div ref={ref} />;
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -3,21 +3,21 @@ import {
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
 });
-export default () => {
+export default ({ history }) => {
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
           <Route path="/" component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
