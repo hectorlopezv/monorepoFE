5848c01997a5be59a4046a770ab01d1d25112405
hvlopez <hectorvmlopez@gmail.com>
2023-05-22 21:30:07 -0600

    add

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
     new moduleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@https://${domain}/marketing/latest/remoteEntry.js`,
+        marketing: `marketing@https://d1411vm7r8y7w3.cloudfront.net/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
